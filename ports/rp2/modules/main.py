6370250081c9e2704bfd0a082edde9922ca114a4
jaylinwylie <jaylinwylie@gmail.com>
2025-03-03 15:30:56 -0800

    cleanup

--- ports/rp2/modules/main.py
+++
@@ -1,8 +0,0 @@
-try:
-    from pixel_driver import EPD
-    EPD().run_scene()
-except Exception as e:
-    print("Critical Error", e)
-    from rgb_logger import RGBLogger
-    log = RGBLogger()
-    log.system_error()
